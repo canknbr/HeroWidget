2d08692002fc5ec2655032381f02c8762a76e6d4
canknbr <cankanbur1@gmail.com>
2023-04-06 21:39:12 +0300

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Can Kanbur on 6.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
